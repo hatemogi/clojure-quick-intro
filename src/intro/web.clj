c98a82ea11964a997bcb9d180b644fa781d1e6ff
Daehyun Kim <hatemogi@gmail.com>
2017-11-10 23:36:01 +0900

    웹 예제 추가

--- src/intro/web.clj
+++ src/intro/web.clj
@@ -1,7 +1,9 @@
 (ns intro.web
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
 (def 가짜요청 (mock/request :get "/"))
 
@@ -9,8 +11,18 @@
   {:status 200
    :body "안녕하세요, 클로저 웹앱입니다."})
 
+(defroutes 라우터
+  (GET "/" [] 핸들러)
+  (POST "/echo-name" [name]
+    (str "안녕하세요, " name "님!"))
+  (route/not-found "찾을 수 없습니다"))
+
+;; $ http :3000/
+;; $ http -v --form POST :3000/echo-name name=김대현
+
 (def 앱
-  (wrap-defaults #'핸들러 site-defaults))
+  (wrap-defaults #'라우터
+     (dissoc site-defaults :security)))
 
 (defn 웹서버실행
   [포트]
